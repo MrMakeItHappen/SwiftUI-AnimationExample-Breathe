b2b68ac41e298c28b577bc26826d9b03484c8b44
Stanley Miller <smiller8203@gmail.com>
2021-04-05 11:24:02 -0400

    Initial Commit

---
+++ Practice-Animations01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Practice-Animations01
+//
+//  Created by Stanley Miller on 4/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Practice-Animations01/Practice_Animations01App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_Animations01App.swift
+//  Practice-Animations01
+//
+//  Created by Stanley Miller on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Practice_Animations01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
